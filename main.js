83724a55928841b2c73a6572fca92005e860b629
Jody Lin <jody.m.lin@gmail.com>
2021-03-09 21:47:01 -0800

    make skybox gradient from blue to white

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 import {defs, tiny} from './examples/common.js';
-import { Seat, WaterTile } from './objects/index.js';
+import { Seat, WaterTile, SkyBox } from './objects/index.js';
 
 const {
     Vector, Vector3, vec, vec3, vec4, color, hex_color, Shader, Matrix, Mat4, Light, Shape, Material, Scene,
@@ -23,6 +23,8 @@ export class Main extends Scene {
         this.initial_camera_location = Mat4.look_at(vec3(0, 10, 20), vec3(0, 0, 0), vec3(0, 1, 0));
         this.seat = new Seat();
         this.water_tile = new WaterTile();
+
+        this.sky_box = new SkyBox();
     }
 
     make_control_panel() {
@@ -61,5 +63,7 @@ export class Main extends Scene {
         // Displaying custom objects
         this.seat.render(context, program_state, 5);
         this.water_tile.render(context, program_state, 5, 5);
+
+        this.sky_box.render(context, program_state);
     }
 }
--- objects/SkyBox.js
+++ objects/SkyBox.js
@@ -67,12 +67,21 @@ vertex_glsl_code() {
 				// ********* FRAGMENT SHADER *********
 				// TODO:  Complete the main function of the fragment shader (Extra Credit Part II).
 				return this.shared_glsl_code() + `
+                varying vec2 f_tex_coord;
 				void main(){
-            float sigmoid = 1. / (1. + pow(2.71828, -0.035 * point_position.y));
-            float blue_color = (sigmoid * 70. + 108.) / 256.;
-            float red_color = (sigmoid * 34. + 48.) / 256.;
-            float green_color = (sigmoid * 100. + 156.) / 256.;
-						gl_FragColor = vec4( red_color, blue_color, green_color, 1.);
+                    
+                    // vec3 texcoord = normalize(f_tex_coord);
+                    // vec3 up = vec3(0., 1., 0.);
+                    // float d = dot(texcoord, up);
+                    // float s = sign(d);
+
+                    // gl_FragColor = vec4(mix(vec4(1.0, 1.0, 0.8), s < 0.0 ? vec3(0.3, 0.3, 1, 0) : vec3(1, 0.3, 0.3, 0), pow(abs(d), 1.)), 1);
+
+                    float sigmoid = 1. / (1. + pow(2.71828, -0.015 * (point_position.y + pow(2., -1. * pow(point_position.x + point_position.y, 2.)))));
+                    float blue_color = (sigmoid * 148. + 108.) / 256.;
+                    float red_color = (sigmoid * 218. + 48.) / 256.;
+                    float green_color = (sigmoid * 100. + 156.) / 256.;
+				    gl_FragColor = vec4( red_color, blue_color, green_color, 1.);
 				}`;
 		}
 }
