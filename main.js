19cecc41664f6fb5da7f062dd3dac5dd5a0ce5bc
shirlyfang <shirly.is.here@gmail.com>
2021-03-11 11:42:04 -0800

    camera controls

--- main.js
+++ main.js
@@ -52,29 +52,27 @@ export class Main extends Scene {
             this.trainMove = !this.trainMove;
         });
         this.new_line();
-        if (!this.detached) {
-            this.key_triggered_button("Look left", ["a"], () => {
-                if (this.horizontal_look > -9) {
-                  this.horizontal_look -= 40 * this.dt;
-                }
-            })
-            this.key_triggered_button("Look right", ["d"], () => {
-                if (this.horizontal_look < 9) {
-                    this.horizontal_look += 40 * this.dt;
-                }
-            });
-            this.key_triggered_button("Look up", ["w"], () => {
-                if (this.vertical_look < 7) {
-                    this.vertical_look += 40 * this.dt;
-                }
-            })
-            this.key_triggered_button("Look down", ["s"], () => {
-                if (this.vertical_look > -3) {
-                    this.vertical_look -= 40 * this.dt;
-                }
-            });
-            this.new_line();
-        }
+        this.key_triggered_button("Look left", ["a"], () => {
+            if (this.horizontal_look > -9) {
+                this.horizontal_look -= 20 * this.dt;
+            }
+        })
+        this.key_triggered_button("Look right", ["d"], () => {
+            if (this.horizontal_look < 9) {
+                this.horizontal_look += 20 * this.dt;
+            }
+        });
+        this.key_triggered_button("Look up", ["w"], () => {
+            if (this.vertical_look < 7) {
+                this.vertical_look += 20 * this.dt;
+            }
+        })
+        this.key_triggered_button("Look down", ["s"], () => {
+            if (this.vertical_look > -3) {
+                this.vertical_look -= 20 * this.dt;
+            }
+        });
+        this.new_line();
         this.key_triggered_button("Detach", ["y"], () => {
             this.detached = true;
         });
@@ -96,13 +94,8 @@ export class Main extends Scene {
         }
         if (!context.scratchpad.controls) {
             this.children.push(context.scratchpad.controls = new defs.Movement_Controls());
-            // Define the global camera and projection matrices, which are stored in program_state.
-            program_state.set_camera(this.initial_camera_location);
-        } else {
-            // Define the global camera and projection matrices, which are stored in program_state.
-            program_state.set_camera(this.initial_camera_location);
         }
-
+        program_state.set_camera(this.initial_camera_location);
         program_state.projection_transform = Mat4.perspective(
             Math.PI / 4, context.width / context.height, .1, 1000);
 
